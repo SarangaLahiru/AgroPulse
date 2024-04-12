9f697abc2692a8bc988dc8b239041c5229f4aa30
SarangaLahiru <agslwijekoon@std.appsc.sab.ac.lk>
2024-04-12 19:52:28 +0530

    update9

--- Mini-Project/Frondend/src/main.jsx
+++ Mini-Project/Frondend/src/main.jsx
@@ -10,12 +10,15 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { ContextProvider } from './context/contextProvider.jsx'
 import router from './Routes.jsx';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ContextProvider>
+      <GoogleOAuthProvider clientId="796659410119-6p76ghbvl4tmcpmngk1v97u8h0n2g6d0.apps.googleusercontent.com">
       <RouterProvider router={router}/>
+      </GoogleOAuthProvider>
       <ToastContainer/> 
     </ContextProvider>
   </React.StrictMode>,
