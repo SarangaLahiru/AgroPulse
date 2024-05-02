54dd2462c7de6d20e9231e4a1f8b866ec6ab0463
SarangaLahiru <agslwijekoon@std.appsc.sab.ac.lk>
2024-05-02 08:15:40 +0530

    update15

---
+++ Mini-Project/Frondend/src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./ContactForm.css";
+
+function ContactForm() {
+  return (
+    <div className="contact  container">
+      <div className="row">
+        <div className="col__2">
+          <div className="contact__box">
+            <div className="contact__meta">
+              <h2 className="hire__text">Support</h2>
+              <h1 className="hire__text">Contact</h1>
+              <p className="hire__text">
+                If you have any questions, problems, or feedback get in touch
+                here:
+              </p>
+            </div>
+
+            <div className="input__box">
+              <div className="contact_sub_text">Name *</div>
+              <input
+                type="text"
+                className="contact"
+                placeholder="Enter Your Name"
+              />
+              <div className="contact_sub_text">Email *</div>
+              <input
+                type="text"
+                className="contact"
+                placeholder="Enter Your Email"
+              />
+              <div className="contact_sub_text">Message *</div>
+              <textarea
+                name="message"
+                id="message"
+                placeholder="Enter Your Massage..."
+              ></textarea>
+              <div className="send__button">
+                <button className="send__pointer btn">send</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ContactForm;
--- Mini-Project/Frondend/src/pages/login.jsx
+++ Mini-Project/Frondend/src/pages/login.jsx
@@ -9,19 +9,20 @@ import axioaClient from '../axios-Client';
 import { useStateContext } from '../context/contextProvider';
 
 export default function Login() {
-  
+
   const idRef = createRef();
   const passwordRef = createRef();
-  const {setToken,setUser}=useStateContext();
+  const { setToken, setUser } = useStateContext();
+  setToken(123)
 
   const handleSubmit = (ev) => {
     ev.preventDefault();
 
     const payload = {
-     
+
       id: idRef.current.value,
       password: passwordRef.current.value,
-     
+
     };
 
     axioaClient.post('/signin', payload)
@@ -33,20 +34,20 @@ export default function Login() {
         setUser(response.data.user)
         console.log(response.data.user)
 
-        
+
       })
       .catch(error => {
         console.log(error.response.data.error); // Handle error
         console.log("dsdfsdf")
         toast.error(error.response.data.error)
-        
+
       });
   };
-    return (
-      <>
-        
-       <FadeIn>
-       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 ">
+  return (
+    <>
+
+      <FadeIn>
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 ">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
               className="mx-auto"
@@ -58,52 +59,52 @@ export default function Login() {
               Sign in
             </h2>
           </div>
-  
+
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form className="space-y-6" action="#" method="POST" onSubmit={handleSubmit}>
-           
-          <FadeIn>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              label="ID Number"
-              name="ID"
-              type="text"
-              className='textInput'
-              color="success"
-              inputRef={idRef}
-            />
-          </Grid>
-          </FadeIn>
-          
-          <FadeIn>
-          
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              label="Password"
-              name="Password"
-              type="password"
-              className='textInput'
-              color="success"
-              inputRef={passwordRef}
-            />
-          </Grid>
-          </FadeIn>
-          
-          
-            
-  
+
               <FadeIn>
-              <div>
-                <Button type='submit' sx={{ color:"white",padding: "12px 18px",bgcolor:"#014802" }} id="loginbtn"
-                className="loginbtn flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 loginbtn"
-                
-                >Sign in</Button>
-              </div>
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    label="ID Number"
+                    name="ID"
+                    type="text"
+                    className='textInput'
+                    color="success"
+                    inputRef={idRef}
+                  />
+                </Grid>
+              </FadeIn>
+
+              <FadeIn>
+
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    label="Password"
+                    name="Password"
+                    type="password"
+                    className='textInput'
+                    color="success"
+                    inputRef={passwordRef}
+                  />
+                </Grid>
+              </FadeIn>
+
+
+
+
+              <FadeIn>
+                <div>
+                  <Button type='submit' sx={{ color: "white", padding: "12px 18px", bgcolor: "#014802" }} id="loginbtn"
+                    className="loginbtn flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 loginbtn"
+
+                  >Sign in</Button>
+                </div>
               </FadeIn>
             </form>
-  
+
             <p className="mt-10 text-center text-sm text-gray-500">
               If you not a member?{' '}
               <a href="/signup" className="font-semibold leading-6 text-green-600 hover:text-green-500">
@@ -112,9 +113,8 @@ export default function Login() {
             </p>
           </div>
         </div>
-       </FadeIn>
+      </FadeIn>
 
-      </>
-    )
-  }
-  
+    </>
+  )
+}
