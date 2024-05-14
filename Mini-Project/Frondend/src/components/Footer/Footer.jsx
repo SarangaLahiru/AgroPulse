a61fe3ba35aa6f0c59b2ea1e30e3763dd0c1bf85
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-05-15 03:05:58 +0530

    Footer-update

--- Mini-Project/Frondend/src/components/Footer/Footer.jsx
+++ Mini-Project/Frondend/src/components/Footer/Footer.jsx
@@ -1,78 +1,81 @@
-import React, { useEffect } from 'react';
-import { FaLocationDot, FaPhone } from "react-icons/fa6";
-import { FiChevronRight } from 'react-icons/fi';
-import { MdEmail } from "react-icons/md";
+import React from 'react';
+import { useEffect } from 'react';
 import './footer.css';
+import { FiChevronRight } from 'react-icons/fi';
+import { FaLocationDot } from 'react-icons/fa6';
+import { FaPhone } from 'react-icons/fa6';
+import { MdEmail } from 'react-icons/md';
+import logo from '../../../public/images/logo2.png';
 
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 
 const Footer = () => {
+  //scroll animation...
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
 
-    //scroll animation...
-    useEffect(() => {
-        Aos.init({ duration: 2000 })
-    }, [])
+  return (
+    <section className='footer' data-aos='fade-up' data-aos-duration='4000'>
 
-    return (
-        <section className='footer'>
-            <div className="footerLinks flex" data-aos='fade-up' data-aos-duration='4000'>
-                {/*Group One - Our Services*/}
-                <div className="linkGroup" data-aos='fade-up' data-aos-duration='000'>
-                    <span className="groupTitle">
-                        Our Services
-                    </span>
+        <div className="bottom-logo">
+            <img src={logo} />
+        </div>
+      <div className='footerLinks flex'>
+        {/* Group One - Our Services */}
+        <div className='linkGroup'>
+          
+          <div className='groupTitle'>Our Services</div>
 
-                    <li className="footerList flex">
-                        <FiChevronRight className='icon' />
-                        Pest Detecion
-                    </li>
+          <li className='footerList flex'>
+            <FiChevronRight className='icon' />
+            Pest Detecion
+          </li>
 
-                    <li className="footerList flex">
-                        <FiChevronRight className='icon' />
-                        Solutions
-                    </li>
+          <li className='footerList flex'>
+            <FiChevronRight className='icon' />
+            Solutions
+          </li>
 
-                    <li className="footerList flex">
-                        <FiChevronRight className='icon' />
-                        Community
-                    </li>
-                </div>
+          <li className='footerList flex'>
+            <FiChevronRight className='icon' />
+            Community
+          </li>
 
-                {/*Group Two - Contact Info*/}
-                <div className="linkGroup" data-aos='fade-up' data-aos-duration='4000'>
-                    <span className="groupTitle">
-                        Contact Info
-                    </span>
+          
+        </div>
 
-                    <li className="footerList flex">
-                        <FaLocationDot className='cont-icon' />
-                        No 25, Foster Lane, Colombo 10
-                    </li>
+        
 
-                    <li className="footerList flex">
-                        <FaPhone className='cont-icon' />
-                        +94 11 267 43 43 <br></br>
-                        +94 11 217 24 34
-                    </li>
+        
 
-                    <li className="footerList flex">
-                        <MdEmail className='cont-icon' />
-                        pest.management@agro.hayleys.com
-                    </li>
-                </div>
+        {/* Group Two - Contact Info */}
+        <div className='linkGroup contactInfo'>
+          <div className='groupTitle'>Contact Info</div>
 
+          <li className='footerList'>
+            <FaLocationDot className='cont-icon' />
+            <span>No 25, Foster Lane, Colombo 10</span>
+          </li>
 
+          <li className='footerList'>
+            <FaPhone className='cont-icon' />
+            <div className='phoneNumber'>+94 11 267 43 43  /  +94 11 217 24 34</div>
+          </li>
 
+          <li className='footerList'>
+            <MdEmail className='cont-icon' />
+            <span>pest.management@agro.com</span>
+          </li>
+        </div>
+      </div>
 
-            </div>
-
-            <div className="bottomSection">
-                <h3>AgroPluse © 2024 All Right Reserved</h3>
-            </div>
-
-        </section>
-    );
-}
+      <div className='bottomSection'>
+        <h3>AgroPluse © 2024 All Right Reserved</h3>
+      </div>
+    </section>
+  );
+};
 
 export default Footer;
